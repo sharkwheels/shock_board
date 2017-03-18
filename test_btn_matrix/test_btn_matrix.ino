e036766fb35d89f076bd515c83e0214991b052fd
Nadine Lessio <idoru00@gmail.com>
2017-03-18 13:11:28 -0400

    passing co-ordinates to the matrix
    That sounds cooler than it is, trust me.

--- test_btn_matrix/test_btn_matrix.ino
+++ test_btn_matrix/test_btn_matrix.ino
@@ -1,5 +1,17 @@
 // Reed Switch Matrix Test courtesy of Jane H.
 
+#include <Adafruit_NeoPixel.h>
+#include <Adafruit_NeoMatrix.h>
+
+#define NEOPIN 2
+#define MATRIX_W 4
+#define MATRIX_H 4
+
+Adafruit_NeoMatrix matrix = Adafruit_NeoMatrix(MATRIX_W, MATRIX_H, NEOPIN,
+                            NEO_MATRIX_BOTTOM + NEO_MATRIX_LEFT +
+                            NEO_MATRIX_ROWS + NEO_MATRIX_ZIGZAG,
+                            NEO_GRB + NEO_KHZ800);
+
 // Array of pins for the rows
 int rPins[] = {6,7};
 int rPinsNo = sizeof(rPins) / sizeof(int);
@@ -11,15 +23,17 @@ int cPinsNo = sizeof(cPins) / sizeof(int);
 // [rows][cols]
 int prev[2][2] = {0};
 
-// [ROW][COLUMN]
-int buttonMap[2][2] = {
-	{1,2},
-	{3,4}
+// [ROW][COLUMN][CORRDINATE]
+int buttonMap[2][2][2] = {
+	{{0,0},{1,0}},
+	{{0,1},{1,1}}
 };
 
 int ledPin = 13;
 int roundBtn = 3;
-int currentSquare = 0;
+
+int currentX = 0;
+int currentY = 0;
 
 
 void setup() 
@@ -49,6 +63,11 @@ void setup()
 
   pinMode(ledPin, OUTPUT);
 
+  matrix.begin();
+  matrix.setBrightness(60);
+  matrix.fillScreen(matrix.Color(255, 255, 255));
+  matrix.drawPixel(0,0,matrix.Color(0, 255, 0)); // one green pixel
+
   Serial.println("Setup Done");
 
 }
@@ -66,9 +85,22 @@ void loop()
 				//Serial.println(buttonMap[rowPin][colPin]);
 
 				digitalWrite(ledPin, HIGH);
-				currentSquare = buttonMap[rowPin][colPin];
+				// pull the current co-ordinate of the button pin 
+				// that is registering: 0,0 - 0,1 - 0,2 etc
+				currentX = buttonMap[rowPin][colPin][0];
+				currentY = buttonMap[rowPin][colPin][1];
+
+				// draw a red pixel at the given co-ordinate on the neomatrix
+				matrix.drawPixel(currentX,currentY,matrix.Color(255, 0, 0)); // one red pixel
+
+				// print that shit
 				Serial.print("square: ");
-				Serial.println(currentSquare);
+				Serial.print(currentX);
+				Serial.print(",");
+				Serial.print(currentY);
+				Serial.println();
+
+				// delay
 				delay(200); 
 				
 			}
@@ -79,4 +111,6 @@ void loop()
 
   	digitalWrite(rPins[rowPin], LOW);		
 	}
+matrix.show();
 }
+
