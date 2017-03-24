78cf20b2c4c1d67b5e7f1b3c24398c0587945d84
Nadine Lessio <idoru00@gmail.com>
2017-03-24 15:05:48 -0400

    timers and bools
    - timer for displaying board ID numbers
    - bool for is game active / not active
    - moved some display things into own functions

--- shock_player_logic/shock_player_logic.ino
+++ shock_player_logic/shock_player_logic.ino
@@ -3,6 +3,7 @@
 
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_NeoMatrix.h>
+#include <elapsedMillis.h>
 
 #define NEOPIN 2
 #define MATRIX_W 2
@@ -52,6 +53,11 @@ bool isP1Round = true;			// which player round is it?
 bool shockEnabled = true;		// are they getting free turns?
 int noShockCount = 0;			// count for free turns
 int freeLimit = 2;				// limit
+bool gameActive;
+
+elapsedMillis timer0;
+#define interval 5000
+bool timer0Fired;
 
 //==== board tests ====//
 
@@ -97,213 +103,240 @@ int freeLimit = 2;				// limit
 // ==== board ID patterns ====//
 	int numberOne[2][2]={
 		{1,0},
+		{0,0}
+	};
+	int numberTwo[2][2]={
+		{0,1},
+		{0,0}
+	};
+	int numberThree[2][2]={
+		{0,0},
 		{1,0}
 	};
 
 void setup() 
 {
 	randomSeed(analogRead(A0));
-  Serial.begin(9600);
-  Serial.println("Multiplexed Buttons Test");
+  	Serial.begin(9600);
+  	Serial.println("Multiplexed Buttons Test");
+
+  	gameActive = false;
 
 	// Set the Row pin mode 
 	// these are triggers, so set them as output
-  Serial.println("Setting Row Pins...");
-  for(int i = 0; i < rPinsNo; i++)
-  {
-  	pinMode(rPins[i], OUTPUT);
-  	digitalWrite(rPins[i], LOW);
-  	Serial.println(rPins[i]);
-  }
+  	Serial.println("Setting Row Pins...");
+	for(int i = 0; i < rPinsNo; i++)
+	{
+		pinMode(rPins[i], OUTPUT);
+		digitalWrite(rPins[i], LOW);
+		Serial.println(rPins[i]);
+	}
 
-  //Set the Column Pin Mode (Input)
-  // sensing if trigger is high
-  Serial.println("Setting Column Pins...");
-  for(int i = 0; i < cPinsNo; i++)
-  {
-  	pinMode(cPins[i], INPUT);
-  	Serial.println(cPins[i]);
-  }
+	//Set the Column Pin Mode (Input)
+	// sensing if trigger is high
+	Serial.println("Setting Column Pins...");
+	for(int i = 0; i < cPinsNo; i++)
+	{
+		pinMode(cPins[i], INPUT);
+		Serial.println(cPins[i]);
+	}
 
-  pinMode(ledPin, OUTPUT);
-  pinMode(p1Shock, OUTPUT);
-  pinMode(p2Shock, OUTPUT);
+	pinMode(ledPin, OUTPUT);
+	pinMode(p1Shock, OUTPUT);
+	pinMode(p2Shock, OUTPUT);
 
-  pinMode(resetBtn, INPUT_PULLUP);
+	pinMode(resetBtn, INPUT_PULLUP);
 
-  matrix.begin();
-  matrix.setBrightness(60);
-  matrix.fillScreen(matrix.Color(255, 255, 255));
-  
-  chooseBoard(random(1,4));
-  Serial.println("Setup Done");
+	matrix.begin();
+	matrix.setBrightness(60);
+
+	chooseBoard(random(1,4));
+	timer0 = 0;
+	timer0Fired = false;
+
+	Serial.println("Setup Done");
 
 }
 
 void loop() 
 {
-	// loop through all the pins
-	for(int rowPin = 0; rowPin < rPinsNo; rowPin++)
+	if ((!timer0Fired) && (timer0 > interval)) 
 	{
-		digitalWrite(rPins[rowPin], HIGH);
-		for(int colPin = 0; colPin < cPinsNo; colPin++)
-		{
-			if(HIGH == digitalRead(cPins[colPin]))
-			{
-				//Serial.println(buttonMap[rowPin][colPin]);
-				digitalWrite(ledPin, HIGH);
-				// pull the current co-ordinate of the button pin 
-				currentX = buttonMap[rowPin][colPin][0];
-				currentY = buttonMap[rowPin][colPin][1];
-				int getPatternCode = currentBoard[currentX][currentY];
-				//Serial.println(getPatternCode);
-
-				// if the button is new and has not been tripped (9)
-				if(getPatternCode != 9)
-				{
-					if(isP1Round)
-					{	
-						Serial.println("Player 1: Go");
-						
-
-						// blink P1 LED or something. Some signal. 
-						if(getPatternCode == 0)
-						{
-							// always color the sea red
-							
-							matrix.drawPixel(currentX,currentY,matrix.Color(255,0,0)); 
-
-							if(shockEnabled)
-							{
-								digitalWrite(p1Shock, HIGH);
-								delay(500);
-								digitalWrite(p1Shock, LOW);
-								noShockCount = 0;
-								isP1Round = false;
-							}
-							else
-							{
-								noShockCount++;
-							}
-						}
-						else if(getPatternCode == 1)
-						{	
-							Serial.println("p1: land!");
-							// always color the square p1's color
-							matrix.drawPixel(currentX,currentY,matrix.Color(0, 0, 255));
-							// increase p1's square count
-							p1SquaresTotal++;
-
-							if(shockEnabled)
-							{
-								Serial.println("P1: disabeling shock");
-								shockEnabled = false;
-							}
-							else
-							{	
-								Serial.println("P1: shock already disabled.");
-								noShockCount++;
-								if(noShockCount == freeLimit)
-								{
-									isP1Round = false;
-									shockEnabled = true;
-									noShockCount = 0;
-									Serial.println("P1: Your free turns are over");
-								}
-							}
-						}
-						Serial.print("P1 Squares: ");
-						Serial.print(p1SquaresTotal);
-						Serial.println();
-						Serial.print("noShockCount: ");
-						Serial.print(noShockCount);
-						Serial.println("-----------");
-						Serial.println();	
-					}
-					else
-					{	
-						Serial.println("Player2: Go");
-						
-
-						if(getPatternCode == 0)
-						{
-							// always color the sea red
-
-							matrix.drawPixel(currentX,currentY,matrix.Color(255,0,0)); 
-							if(shockEnabled)
-							{
-								digitalWrite(p2Shock, HIGH);
-								delay(500);
-								digitalWrite(p2Shock, LOW);
-								noShockCount = 0;
-								isP1Round = true;
-							}
-							else
-							{
-								noShockCount++;
-							}
-						}
-						else if(getPatternCode == 1)
-						{
-							matrix.drawPixel(currentX,currentY,matrix.Color(0, 255, 0));
-							p2SquaresTotal++;
-
-							if(shockEnabled)
-							{
-								Serial.println("P2: disabeling shock P2");
-								shockEnabled = false;
-							}
-							else
-							{
-								Serial.println("P2: shock already disabled.");
-								noShockCount++;
-								if(noShockCount == freeLimit)
-								{
-									isP1Round = true;
-									shockEnabled = true;
-									noShockCount = 0;
-									Serial.println("P2: Your free turns are over");
-								}
-							}
-						}
-						Serial.print("p2 squares: ");
-						Serial.print(p2SquaresTotal);
-						Serial.println();
-						Serial.print("noShockCount: ");
-						Serial.print(noShockCount);
-						Serial.println("-----------");
-						Serial.println();
-						
-					}// end of player logic
-					currentBoard[currentX][currentY] = 9;
-				} // end of !9
-				delay(200); 
-			}
-			else 
-			{
-				digitalWrite(ledPin, LOW);
-			}
-  	}
-  	digitalWrite(rPins[rowPin], LOW);		
+		Serial.println("fired");
+  	timer0Fired = true; // don't execute this again
+  	turnWhite();
+  	gameActive = true;
 	}
 
+	if(gameActive)
+	{
+		// loop through all the pins
+		for(int rowPin = 0; rowPin < rPinsNo; rowPin++)
+		{
+			digitalWrite(rPins[rowPin], HIGH);
+			for(int colPin = 0; colPin < cPinsNo; colPin++)
+			{
+				if(HIGH == digitalRead(cPins[colPin]))
+				{
+					//Serial.println(buttonMap[rowPin][colPin]);
+					digitalWrite(ledPin, HIGH);
+					// pull the current co-ordinate of the button pin 
+					currentX = buttonMap[rowPin][colPin][0];
+					currentY = buttonMap[rowPin][colPin][1];
+					int getPatternCode = currentBoard[currentX][currentY];
+					//Serial.println(getPatternCode);
+
+					// if the button is new and has not been tripped (9)
+					if(getPatternCode != 9)
+					{
+						if(isP1Round)
+						{	
+							Serial.println("Player 1: Go");
+							
+
+							// blink P1 LED or something. Some signal. 
+							if(getPatternCode == 0)
+							{
+								// always color the sea red
+								
+								matrix.drawPixel(currentX,currentY,matrix.Color(255,0,0)); 
+
+								if(shockEnabled)
+								{
+									digitalWrite(p1Shock, HIGH);
+									delay(500);
+									digitalWrite(p1Shock, LOW);
+									noShockCount = 0;
+									isP1Round = false;
+								}
+								else
+								{
+									noShockCount++;
+								}
+							}
+							else if(getPatternCode == 1)
+							{	
+								Serial.println("p1: land!");
+								// always color the square p1's color
+								matrix.drawPixel(currentX,currentY,matrix.Color(0, 0, 255));
+								// increase p1's square count
+								p1SquaresTotal++;
+
+								if(shockEnabled)
+								{
+									Serial.println("P1: disabeling shock");
+									shockEnabled = false;
+								}
+								else
+								{	
+									Serial.println("P1: shock already disabled.");
+									noShockCount++;
+									if(noShockCount == freeLimit)
+									{
+										isP1Round = false;
+										shockEnabled = true;
+										noShockCount = 0;
+										Serial.println("P1: Your free turns are over");
+									}
+								}
+							}
+							Serial.print("P1 Squares: ");
+							Serial.print(p1SquaresTotal);
+							Serial.println();
+							Serial.print("noShockCount: ");
+							Serial.print(noShockCount);
+							Serial.println("-----------");
+							Serial.println();	
+						}
+						else
+						{	
+							Serial.println("Player2: Go");
+							
+
+							if(getPatternCode == 0)
+							{
+								// always color the sea red
+
+								matrix.drawPixel(currentX,currentY,matrix.Color(255,0,0)); 
+								if(shockEnabled)
+								{
+									digitalWrite(p2Shock, HIGH);
+									delay(500);
+									digitalWrite(p2Shock, LOW);
+									noShockCount = 0;
+									isP1Round = true;
+								}
+								else
+								{
+									noShockCount++;
+								}
+							}
+							else if(getPatternCode == 1)
+							{
+								matrix.drawPixel(currentX,currentY,matrix.Color(0, 255, 0));
+								p2SquaresTotal++;
+
+								if(shockEnabled)
+								{
+									Serial.println("P2: disabeling shock P2");
+									shockEnabled = false;
+								}
+								else
+								{
+									Serial.println("P2: shock already disabled.");
+									noShockCount++;
+									if(noShockCount == freeLimit)
+									{
+										isP1Round = true;
+										shockEnabled = true;
+										noShockCount = 0;
+										Serial.println("P2: Your free turns are over");
+									}
+								}
+							}
+							Serial.print("p2 squares: ");
+							Serial.print(p2SquaresTotal);
+							Serial.println();
+							Serial.print("noShockCount: ");
+							Serial.print(noShockCount);
+							Serial.println("-----------");
+							Serial.println();
+							
+						}// end of player logic
+						currentBoard[currentX][currentY] = 9;
+					} // end of !9
+					delay(200); 
+				}
+				else 
+				{
+					digitalWrite(ledPin, LOW);
+				}
+	  	}
+	  	digitalWrite(rPins[rowPin], LOW);		
+		}
+	}
+	
 	// the number of squares land squares both players have add up to the 
 	// number of squares in the pattern
 
 	if((p2SquaresTotal + p1SquaresTotal) == currentPatternSize){
 		// do a visual flag for "game is over!"
+		gameActive = false;
 		calculateWinner();
 	}
-
+	
 	// reset button
 	bool newResetState = digitalRead(resetBtn);
-  	if (newResetState == LOW && oldResetState == HIGH) 
-  	{
-  		delay(50);
-  		resetEverything();
-  	}
-		oldResetState = newResetState;
-		matrix.show();
+	if (newResetState == LOW && oldResetState == HIGH) 
+	{	
+		delay(50);
+		resetEverything();
+	}
+	oldResetState = newResetState;
+
+	
+	matrix.show();
+
 }
 //===================== Functions ===================== //
 
@@ -329,38 +362,42 @@ void resetEverything()
 	p1SquaresTotal = 0;
 	p2SquaresTotal = 0;
 	currentPatternSize = 0;
-
-	// wipe the board
-	matrix.fillScreen(matrix.Color(255, 255, 255));
-	// choose a new random board
+	timer0 = 0;
+  timer0Fired = false;
  	chooseBoard(random(1,4));
+ 	
 
 }
 
-void chooseBoard(int command){
+void chooseBoard(int command)
+{
 	if(command == 1)
-	{
-		boardPattern(boardOne, numberOne);
+	{	
+		showNumber(numberOne);
+		loadBoard(boardOne);
 		currentPatternSize = 2;
 	}
 	else if(command == 2)
 	{
-		boardPattern(boardTwo,numberOne);
+		showNumber(numberTwo);
+		loadBoard(boardTwo);
 		currentPatternSize = 2;
+		
 	}
 	else if(command == 3)
 	{
-		boardPattern(boardThree, numberOne);
+		showNumber(numberThree);
+		loadBoard(boardThree);
 		currentPatternSize = 1;
-
 	}	
 	Serial.print("board: ");
 	Serial.print(command);
 	Serial.println();
+	
+
 }
 
-void boardPattern(int pattern[2][2],int IDpattern[2][2])
-{
+void showNumber(int IDpattern[2][2]){
 	// show the players the board #
 	for(int row = 0; row < rPinsNo; row++) 
 	{
@@ -368,15 +405,19 @@ void boardPattern(int pattern[2][2],int IDpattern[2][2])
     	{
      		if(IDpattern[row][column] == 1) 
      		{
-       			matrix.drawPixel(row,column,matrix.Color(200, 0, 200)); 
+       			matrix.drawPixel(row,column,matrix.Color(255, 0, 255)); 
      		} 
+     		else 
+     		{
+     			matrix.drawPixel(row,column,matrix.Color(255, 255, 255)); 
+     		}
    		}
   	}
-  	// pause and then wipe the ID number from the screen
-  	delay(1000);
-  	matrix.fillScreen(matrix.Color(255, 255, 255));
 
-	// assign chosen pattern as current pattern
+}
+
+void loadBoard(int pattern[2][2])
+{
 	for(int i = 0; i < 2; i++)
 	{
 		for(int j = 0; j < 2; j++)
@@ -384,7 +425,15 @@ void boardPattern(int pattern[2][2],int IDpattern[2][2])
 			currentBoard[i][j] = pattern[i][j];
 		}
 	}
-	Serial.println("board loaded");
+
+}
+
+void turnWhite(){
+	Serial.println("turning the whole board white");
+	matrix.fillScreen(matrix.Color(255, 255, 255));
+}
+
+	//
 
 	// for testing patterns
 	// 0 = sea (shock)
@@ -400,9 +449,24 @@ void boardPattern(int pattern[2][2],int IDpattern[2][2])
      } 
    }
   }*/
-	
-}
 
 
+
+/*
+// show the players the board #
+	for(int row = 0; row < rPinsNo; row++) 
+	{
+    	for(int column = 0; column < cPinsNo; column++) 
+    	{
+     		if(IDpattern[row][column] == 1) 
+     		{
+       			matrix.drawPixel(row,column,matrix.Color(255, 0, 255)); 
+     		} 
+   		}
+  	}
+  	// pause and then wipe the ID number from the screen
+  	
+
+*/
 
 
